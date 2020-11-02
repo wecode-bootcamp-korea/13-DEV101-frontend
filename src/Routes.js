560610bd2d8a2adb7c5bf2827231d9fe3bf91cd5
Tania <tania@gimbolaui-MacBookPro-5.local>
2020-11-02 17:28:14 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Router>
+      {/* <Nav /> */}
+      <Switch></Switch>
+      {/* <Footer /> */}
+    </Router>
+  );
+};
+
+export default Routes;
---
+++ store.js
@@ -0,0 +1,44 @@
+import { createAction, configureStore, createReducer } from "@reduxjs/toolkit";
+// const ChangeCategory = createAction('CHANGE_CATEGORY')
+// const CurrentMovie = createAction('CURRENT_MOVIE')
+// const ChangeMovieInfo = createAction('CHANGE_MOVIE_INFO')
+// const RatingLength = createAction('RATING_LENGTH')
+// const ChangeCommentWrite = createAction('CHANGE_COMMENT_WRITE')
+// const CommentValueChange = createAction('COMMENT_VALUE_CHANGE')
+// const initialState = {
+// }
+// const reducer = createReducer(initialState, {
+//   [ChangeCategory] : (state, action) => {
+//     state.category = action.payload
+//   },
+//   [ChangeMovieInfo] : (state, action) => {
+//     console.log(action)
+//     state.isMovieInfo = action.payload
+//   },
+//   [ChangeCommentWrite] : (state, action) => {
+//     console.log(action)
+//     state.isCommentWrite = action.payload
+//   },
+//   [CurrentMovie] : (state, action) => {
+//     state.currentMovie = action.payload
+//   },
+//   [CommentValueChange] : (state, action) => {
+//     state.commentValue = action.payload
+//   },
+//   [RatingLength] : (state, action) => {
+//     state.ratingLength = action.payload
+//   }
+// })
+// export const actionCreators = {
+//   ChangeCategory,
+//   ChangeMovieInfo,
+//   ChangeCommentWrite,
+//   CurrentMovie,
+//   RatingLength,
+//   CommentValueChange
+// }
+// const store = configureStore({
+//   reducer: reducer,
+//   devTools: process.env.NODE_ENV !== 'production',
+// })
+export default store;
