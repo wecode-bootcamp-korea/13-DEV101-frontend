62d84b4a26c75a1007f405eadda861f14ac9142f
sunnysideup0w0v <723973972538@naver.com>
2020-11-04 10:21:49 +0900

    Remove: 불필요 scss제거

---
+++ src/Components/SliderComponent.js
@@ -0,0 +1,32 @@
+import ClassCard from "./ClassCard";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+
+const SliderComponent = ({ dataList, showingSlidesCount }) => {
+  return (
+    <SliderWrapper>
+      <Swiper slidesPerView={showingSlidesCount || 4}>
+        {dataList?.map((data) => {
+          return (
+            <SwiperSlide key={data.product_id}>
+              <ClassCard {...data} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </SliderWrapper>
+  );
+};
+
+const SliderWrapper = styled.div`
+  margin-bottom: 100px;
+  ${Swiper} {
+    .swiper-container {
+      position: relative;
+    }
+  }
+`;
+
+export default SliderComponent;
---
+++ src/Pages/Main/Data.js
@@ -0,0 +1,40 @@
+const mainSliderData = [
+  {
+    id: 1,
+    imgSrc:
+      "https://images.unsplash.com/photo-1542978415-cd8ea8e4c6df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    title: "NEW! <br/> 몽글몽글 파스텔",
+    description: "함께할수록 몽글몽글<br/>마음까지 몽글해지는 파스텔 그리기",
+  },
+  {
+    id: 2,
+    imgSrc:
+      "https://images.unsplash.com/photo-1481143348606-3c0205d25023?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    title: "감각있는 사진 찍기",
+    description:
+      "휴대폰 하나로만 진행하는 감각적인 사진 촬영<br/> 바람 따라 구름 따라 이동해볼까요?",
+  },
+  {
+    id: 3,
+    imgSrc:
+      "https://images.unsplash.com/photo-1596367576552-3da0c8e7e697?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80",
+    title: "캘리그라피,<br/>수채화 함께 배우기",
+    description: "수채화와 함께 그려나가는 글자의 세계.",
+  },
+  {
+    id: 4,
+    imgSrc:
+      "https://images.unsplash.com/photo-1584992236310-6edddc08acff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+    title: "짜임새있는 구조<br/>뜨개질 배우기",
+    description: "겨울철 필수템 목도리를 직접 짜며 배우는 뜨개질",
+  },
+  {
+    id: 5,
+    imgSrc:
+      "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    title: "비전공자도 할 수 있다!<br/>함께라면 두렵지 않은 코딩",
+    description: "html 기조부터 차근차근 배워가는<br/>나만의 웹사이트 만들기 프로젝트",
+  },
+];
+
+export { mainSliderData };
