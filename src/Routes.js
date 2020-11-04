db7127cddce704a26ef4a5261df1f44beebe5cbc
sunnysideup0w0v <723973972538@naver.com>
2020-11-04 11:16:31 +0900

    Fix : 개발환경 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./Styles/common";
 
 const Routes = () => {
   return (
-    <Router>
-      {/* <Nav /> */}
-      <Switch></Switch>
-      {/* <Footer /> */}
-    </Router>
+    <Provider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          {/* <Nav /> */}
+          <Switch></Switch>
+          {/* <Footer /> */}
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
