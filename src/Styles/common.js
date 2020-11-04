48b6880403516bc9f6cd8e1d98a372c83c8923bf
sunnysideup0w0v <723973972538@naver.com>
2020-11-04 11:16:31 +0900

    Fix : 개발환경 수정

---
+++ src/Styles/common.js
@@ -0,0 +1,10 @@
+const theme = {
+  contentWrapperWidth: "1176px",
+  itemWidth: "276px",
+  titleBlackColor: "#1b1c1d",
+  textGrayColor: "#858a8d",
+  orangeColor: "#ff922b",
+  orangeHoverColor: "#f77800",
+};
+
+export default theme;
