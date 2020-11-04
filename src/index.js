a8a1255b0a58a2b8abf338404174690078786394
sunnysideup0w0v <723973972538@naver.com>
2020-11-04 18:52:34 +0900

    Modify: classcard 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import "./Styles/reset.scss";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
