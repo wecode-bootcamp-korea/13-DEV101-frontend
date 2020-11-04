f05a2b6c1208fb6111418e8a46505896eaeec950
sunnysideup0w0v <723973972538@naver.com>
2020-11-04 18:52:34 +0900

    Modify: classcard 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import ClassCard from "./Components/ClassCard";
 import { ThemeProvider } from "styled-components";
 import theme from "./Styles/common";
 
 const Routes = () => {
   return (
-    <Provider>
-      <ThemeProvider theme={theme}>
-        <Router>
-          {/* <Nav /> */}
-          <Switch></Switch>
-          {/* <Footer /> */}
-        </Router>
-      </ThemeProvider>
-    </Provider>
+    // <Provider>
+    <ThemeProvider theme={theme}>
+      <Router>
+        {/* <Nav /> */}
+        <Switch>
+          <Route path="/" component={ClassCard} />
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
+    </ThemeProvider>
+    // </Provider>
   );
 };
 
